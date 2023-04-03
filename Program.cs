bebd5ffe23222d76e923c36ac41979d904f4c450
hoan47 <b2h1604bmt@gmail.com>
2023-04-03 16:11:11 +0700

    exercise codegym

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime timeSystem = DateTime.Now;
+            System.Console.WriteLine("Datetime Now is :" + timeSystem);
+        }
+    }
+}
