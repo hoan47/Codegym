309aa16979e109771892b07a145d2c76a61362e7
hoan47 <b2h1604bmt@gmail.com>
2023-04-03 16:57:14 +0700

    Phuong trinh bac nhat

--- Program.cs
+++ Program.cs
@@ -7,9 +7,26 @@ namespace Test01
         static void Main(string[] args)
         {
 
-            float USD;
-            USD = float.Parse(Console.ReadLine());
-            Console.WriteLine(USD * 23000);
+            Console.WriteLine("Linear Equation Resolver");
+            Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+            double a, b;
+            Console.Write("a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            if(a==0)
+                if(b==0)
+                {
+                    Console.Write("The solution is all x!");
+                }
+                else
+                {
+                    Console.Write("Nod solution!");
+                }
+            else
+            {
+                Console.Write("The solution is: {0}!", -b/a);
+            }
         }
     }
 }
