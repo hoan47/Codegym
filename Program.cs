1e36af398695b371663027c5aeb08ed67bb4f926
hoan47 <b2h1604bmt@gmail.com>
2023-04-04 20:49:51 +0700

    Read number

--- Program.cs
+++ Program.cs
@@ -6,24 +6,77 @@ namespace Test01
     {
         static void Main(string[] args)
         {
-            float weight, height;
-            Console.WriteLine("Please enter a height");
-            weight = float.Parse(Console.ReadLine());
-
-            height = float.Parse(Console.ReadLine());
-
-            float bmi;
-            bmi = weight / height;
-            bmi = (float)Math.Round(bmi, 1);
-            Console.Write("BMI: " + bmi);
-            if (bmi < 18.5)
-                Console.WriteLine(" Underweight");
-            else if (18.5 <= bmi && bmi < 25)
-                Console.WriteLine(" Normal");
-            else if (25 <= bmi && bmi < 30)
-                Console.WriteLine(" Overweight");
+            string s;
+            s = Console.ReadLine();
+            if (s.Length == 1)
+            {
+                One(s);
+            }
+            else if(s.Length == 2)
+            {
+                Two(s);
+            }
             else
-                Console.WriteLine(" Obese");
+            {
+                Three(s);
+            }
+        }
+        static void One(string s)
+        {
+            Console.WriteLine(Read(char.Parse(s)));
+        }
+        static void Two(string s)
+        {
+            if(int.Parse(s)<20)
+            {
+                Console.Write($"{Read(s[1])}teen");
+            }
+            else
+            {
+                Console.Write($"{Read(s[0])}ty {Read(s[1])}");
+            }
+        }
+        static void Three(string s)
+        {
+            Console.Write($"{Read(s[0])} hundred and ");
+            Two(s.Substring(1, 2));
+        }
+        static string Read(char s)
+        {
+            switch (s)
+            {
+                case '0':
+                    return "zero";
+                    break;
+                case '1':
+                    return "one";
+                    break;
+                case '2':
+                    return "two";
+                    break;
+                case '3':
+                    return "three";
+                    break;
+                case '4':
+                    return "four";
+                    break;
+                case '5':
+                    return "five";
+                    break;
+                case '6':
+                    return "six";
+                    break;
+                case '7':
+                    return "seven";
+                    break;
+                case '8':
+                    return "eight";
+                    break;
+                case '9':
+                    return "nine";
+                    break;
+            }
+            return "0";
         }
     }
 }
