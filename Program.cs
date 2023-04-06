1cf52a15ea882088e56e3c4f626100e7f3b20122
hoan47 <b2h1604bmt@gmail.com>
2023-04-06 19:04:18 +0700

    menu

--- Program.cs
+++ Program.cs
@@ -6,77 +6,50 @@ namespace Test01
     {
         static void Main(string[] args)
         {
-            string s;
-            s = Console.ReadLine();
-            if (s.Length == 1)
+            int choice = -1;
+            while (choice != 0)
             {
-                One(s);
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Draw the triangle");
+                Console.WriteLine("2. Draw the square");
+                Console.WriteLine("3. Draw the rectangle");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Enter your choice: ");
+                choice = Int32.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Draw the triangle");
+                        Console.WriteLine("******");
+                        Console.WriteLine("*****");
+                        Console.WriteLine("****");
+                        Console.WriteLine("***");
+                        Console.WriteLine("**");
+                        Console.WriteLine("*");
+                        break;
+                    case 2:
+                        Console.WriteLine("Draw the square");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        break;
+                    case 3:
+                        Console.WriteLine("Draw the rectangle");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        Console.WriteLine("* * * * * *");
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("No choice!");
+                        break;
+                }
             }
-            else if(s.Length == 2)
-            {
-                Two(s);
-            }
-            else
-            {
-                Three(s);
-            }
-        }
-        static void One(string s)
-        {
-            Console.WriteLine(Read(char.Parse(s)));
-        }
-        static void Two(string s)
-        {
-            if(int.Parse(s)<20)
-            {
-                Console.Write($"{Read(s[1])}teen");
-            }
-            else
-            {
-                Console.Write($"{Read(s[0])}ty {Read(s[1])}");
-            }
-        }
-        static void Three(string s)
-        {
-            Console.Write($"{Read(s[0])} hundred and ");
-            Two(s.Substring(1, 2));
-        }
-        static string Read(char s)
-        {
-            switch (s)
-            {
-                case '0':
-                    return "zero";
-                    break;
-                case '1':
-                    return "one";
-                    break;
-                case '2':
-                    return "two";
-                    break;
-                case '3':
-                    return "three";
-                    break;
-                case '4':
-                    return "four";
-                    break;
-                case '5':
-                    return "five";
-                    break;
-                case '6':
-                    return "six";
-                    break;
-                case '7':
-                    return "seven";
-                    break;
-                case '8':
-                    return "eight";
-                    break;
-                case '9':
-                    return "nine";
-                    break;
-            }
-            return "0";
         }
     }
 }
