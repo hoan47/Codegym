67a21a318dadf28dc8f2fd1908b6ed593b9e1f01
hoan47 <b2h1604bmt@gmail.com>
2023-04-06 19:16:03 +0700

    prime

--- Program.cs
+++ Program.cs
@@ -6,50 +6,17 @@ namespace Test01
     {
         static void Main(string[] args)
         {
-            int choice = -1;
-            while (choice != 0)
+            int n;
+            n = int.Parse(Console.ReadLine());
+            for(int i=2; i<=Math.Sqrt(n); i++)
             {
-                Console.WriteLine("Menu");
-                Console.WriteLine("1. Draw the triangle");
-                Console.WriteLine("2. Draw the square");
-                Console.WriteLine("3. Draw the rectangle");
-                Console.WriteLine("0. Exit");
-                Console.WriteLine("Enter your choice: ");
-                choice = Int32.Parse(Console.ReadLine());
-                switch (choice)
+                if(n%i==0)
                 {
-                    case 1:
-                        Console.WriteLine("Draw the triangle");
-                        Console.WriteLine("******");
-                        Console.WriteLine("*****");
-                        Console.WriteLine("****");
-                        Console.WriteLine("***");
-                        Console.WriteLine("**");
-                        Console.WriteLine("*");
-                        break;
-                    case 2:
-                        Console.WriteLine("Draw the square");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        break;
-                    case 3:
-                        Console.WriteLine("Draw the rectangle");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        Console.WriteLine("* * * * * *");
-                        break;
-                    case 0:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("No choice!");
-                        break;
+                    Console.WriteLine($"{n} is not a prime");
+                    return;
                 }
             }
+            Console.WriteLine($"{n} is a prime");
         }
     }
 }
