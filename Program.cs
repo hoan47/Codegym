8e5d3e1a46258e2d102ff6a8a9628b3b8f72e375
hoan47 <b2h1604bmt@gmail.com>
2023-04-08 18:36:03 +0700

    sum array

--- Program.cs
+++ Program.cs
@@ -6,68 +6,23 @@ namespace Test01
     {
         static void Main(string[] args)
         {
-            int choice = -1, n, m;
-            while (choice != 0)
+            int[] numbers = new int[5];
+            numbers[0] = 2;
+            numbers[1] = 5;
+            numbers[2] = 9;
+            numbers[3] = 6;
+            numbers[4] = 7;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[2]);
+            Console.WriteLine(numbers[3]);
+            int total = 0;
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine("Menu");
-                Console.WriteLine("1. Draw the triangle");
-                Console.WriteLine("2. Draw the square");
-                Console.WriteLine("3. Draw the rectangle");
-                Console.WriteLine("0. Exit");
-                Console.WriteLine("Enter your choice: ");
-                choice = Int32.Parse(Console.ReadLine());
-                switch (choice)
-                {
-                    case 1:
-                        Console.Write("Draw the triangle\nSquared angle: ");
-                        n = int.Parse(Console.ReadLine());
-                        for(int i=0, j=1, k=2; i<n*(n+1)/2; i++)
-                        {
-                            if(i==j)
-                            {
-                                Console.WriteLine();
-                                j = k * (k + 1) / 2; k++;
-                            }
-                            Console.Write("* ");
-                        }
-                        Console.WriteLine();
-                        break;
-                    case 2:
-                        Console.Write("Draw the square\nSquare edge: ");
-                        n = int.Parse(Console.ReadLine());
-                        for(int i=0; i<n*n; i++)
-                        {
-                            if (i % n == 0 && i != 0) 
-                            {
-                                Console.WriteLine();
-                            }
-                            Console.Write("* ");
-                        }
-                        Console.WriteLine();
-                        break;
-                    case 3:
-                        Console.Write("Draw the rectangle\nWidth: ");
-                        n = int.Parse(Console.ReadLine());
-                        Console.Write("Hight: ");
-                        m = int.Parse(Console.ReadLine());
-                        for (int i = 0; i < n * m; i++)
-                        {
-                            if (i % m == 0)
-                            {
-                                Console.WriteLine();
-                            }
-                            Console.Write("* ");
-                        }
-                        Console.WriteLine();
-                        break;
-                    case 0:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("No choice!");
-                        break;
-                }
+                total = total + numbers[i];
             }
+            Console.WriteLine(total.ToString());
+
         }
     }
 }
