4b69218f210433a160faf8c98c6e8e45e5ed48e7
hoan47 <b2h1604bmt@gmail.com>
2023-04-11 18:07:20 +0700

    max

--- Program.cs
+++ Program.cs
@@ -6,27 +6,22 @@ namespace Test01
     {
         static void Main(string[] args)
         {
-            int m = int.Parse(Console.ReadLine());
-            int[] a = new int[m];
-            string[] s = Console.ReadLine().Split(' ');
-            for (int i = 0; i < m; i++)
+            int m, n, max;
+            max = int.MinValue;
+            m = int.Parse(Console.ReadLine());
+            n = int.Parse(Console.ReadLine());
+            int[,] a = new int[m, n];
+            for(int i=0; i<m; i++)
             {
-                a[i] = int.Parse(s[i]);
+                string[] s = Console.ReadLine().Split(' ');
+                for(int j=0; j<n; j++)
+                {
+                    a[i, j] = int.Parse(s[j]);
+                    if (a[i, j] > max)
+                        max = a[i, j];
+                }
             }
-            int X = int.Parse(Console.ReadLine());
-            int index = int.Parse(Console.ReadLine());
-            if(index<=1 || a.Length-1<=index)
-            {
-                Console.WriteLine("Khong chen duoc");
-                return;
-            }    
-            Array.Resize(ref a, a.Length + 1);
-            Array.Copy(a, index, a, index + 1, a.Length - 1 - index);
-            a[index] = X;
-            for (int i = 0; i < a.Length; i++) 
-            {
-                Console.Write($"{a[i]} ");
-            }   
+            Console.WriteLine(max);
         }
     }
 }
